d6102aaa97af5dc0a0eb54a32b3790c9681e502b
JoseMeurer <josewm18@gmail.com>
2022-09-26 20:01:31 -0300

    URI 2990

--- uri2990/src/main/java/com/devsuperior/uri2990/Uri2990Application.java
+++ uri2990/src/main/java/com/devsuperior/uri2990/Uri2990Application.java
@@ -1,11 +1,15 @@
 package com.devsuperior.uri2990;
 
+import com.devsuperior.uri2990.dto.EmpregadoDeptDTO;
+import com.devsuperior.uri2990.projections.EmpregadoDeptProjection;
+import com.devsuperior.uri2990.repositories.EmpregadoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.devsuperior.uri2990.repositories.EmpregadoRepository;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 public class Uri2990Application implements CommandLineRunner {
@@ -19,6 +23,15 @@ public class Uri2990Application implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+
+
+		List<EmpregadoDeptProjection> listSQL = repository.searchSQL();
+		List<EmpregadoDeptDTO> listDTO = listSQL.stream().map(EmpregadoDeptDTO::new).collect(Collectors.toList());
+		System.out.println("\n--SQL--");
+		listDTO.forEach(System.out::println);
+
+		List<EmpregadoDeptDTO> listJPQL = repository.searchJPQL();
+		System.out.println("\n--JPQL--");
+		listJPQL.forEach(System.out::println);
 	}
 }
